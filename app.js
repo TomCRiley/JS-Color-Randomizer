e51db82ee5eaaab562927def314f8a7865e22645
Tom Riley <tom.riley@outlook.com>
2022-08-21 09:05:07 +0100

    refactored to make color generator its own func()

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const button = document.querySelector('button');
 const h1 = document.querySelector('h1');
+
 button.addEventListener('click', () => {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  const newColor = `rgb(${r}, ${g}, ${b})`;
-  console.log(newColor);
+  const newColor = makeRandColor();
   document.body.style.backgroundColor = newColor;
   h1.innerText = newColor;
 });
+
+const makeRandColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
